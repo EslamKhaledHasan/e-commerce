fabba7f64d7317669a7d93e2efd994f9cfd8110c
eslaamm66 <you@example.com>
2022-11-23 09:48:57 +0200

    first commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center( child: Column(children: [TextField(
+    keyboardType: TextInputType.text,
+    obscureText: true,
+    decoration: InputDecoration(
+      hintText: "Enter Your Password : ",
+      // To delete borders
+      enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
+      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
+      // fillColor: Colors.red,
+      filled: true,
+      contentPadding: const EdgeInsets.all(8),
+    ))]),
+    ));
+  }
+}
