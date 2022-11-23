16aacc8c605ecd08f1c58d9b3be0f1eeff410134
eslaamm66 <you@example.com>
2022-11-23 11:27:12 +0200

    create password textbox

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,18 +6,33 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center( child: Column(children: [TextField(
-    keyboardType: TextInputType.text,
-    obscureText: true,
-    decoration: InputDecoration(
-      hintText: "Enter Your Password : ",
-      // To delete borders
-      enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
-      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
-      // fillColor: Colors.red,
-      filled: true,
-      contentPadding: const EdgeInsets.all(8),
-    ))]),
+        body: Center(
+      child: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: const SizedBox(
+            height: 62,
+          ),
+        ),
+        TextField(
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "Enter Your Password : ",
+              // To delete borders
+              enabledBorder: OutlineInputBorder(
+                borderSide: Divider.createBorderSide(context),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey,
+                ),
+              ),
+              // fillColor: Colors.red,
+              filled: true,
+              contentPadding: const EdgeInsets.all(8),
+            ))
+      ]),
     ));
   }
 }
